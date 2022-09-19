c12bb2d8779793f8a1dcb5a824c04dd1562e25c0
alehshylin <ol564zx@gmail.com>
2022-09-19 22:26:20 +0300

    Major changes were performed in the code and commentary part of the 'Collections.py' file

--- 2_module_Collections/Collections.py
+++ 2_module_Collections/Collections.py
@@ -1,7 +1,6 @@
 # To enable working with random numbers we need to import library 'random'
 import random
 
-
 # Home task 1 - create a list of random number of dicts (from 2 to 10)
 
 # Before append dicts to the list let's create list
@@ -37,32 +36,48 @@ for i in range(random.randint(2, 10)):
 # Let's print our list
 print(list_with_dicts, '\n')
 
-
 # Home task 2 - get previously generated list of dicts and create one common dict
 
 # Let's create empty dict firstly
 result_dict = {}
+# And related for this dict lists, result_dict_keys - for keys in the future dict;
+# result_dict_values - for values in the future dict;
+# result_dict_keys_temporary - to transformation composite keys to one symbol (i.e. from 'x_2' to 'x')
+result_dict_keys = []
+result_dict_values = []
+result_dict_keys_temporary = []
 
 # First for... statement was created for list parsing
 for i in range(len(list_with_dicts)):
     # Second for... statement was created for dict parsing. By accessing list by index we can work with the dict.
     for key in list_with_dicts[i]:
-        # In this if... statement we check that key from dict from list (from the first task) is exists in the
-        # new dict (result_dict)
-        if key in result_dict:
+        # In this case I parse keys from list with true keys and convert them to one symbol (i.e. from 'q_2' to 'q')
+        for j in range(len(result_dict_keys)):
+            result_dict_keys_temporary[j] = result_dict_keys[j][:1]
+        # I perform the convertation to one symbol for this if... statement. Because all keys from old dict are as one symbol
+        if key in result_dict_keys_temporary:
+            # I take index from dict_keys list to send it to the dict_values list because one position in both list
+            # corresponds to the one key:value pars in the dict
+            result_dict_index = result_dict_keys_temporary.index(key)
             # If key already exists in the new dict, we compare values from old dict and the new dict.
-            if list_with_dicts[i][key] > result_dict[key]:
+            if list_with_dicts[i][key] > result_dict_values[result_dict_index]:
                 # If value from the old dict is greater than value from new dict, we create key as key_{number_of_dict}.
                 # Please note, that dict numeration is going from 0.
                 # So, that means that 'q_1' key is from the 2 dict, not the first.
                 index_for_new_dict = str(key) + '_' + str(i)
-                # We delete key:value pars from the new dict by the old key name and old value
-                result_dict.pop(key)
-                # And add key:value pars with the new key name and value
-                result_dict[index_for_new_dict] = list_with_dicts[i][key]
+                # And I rewrite values in all three lists
+                result_dict_keys[result_dict_index] = index_for_new_dict
+                result_dict_values[result_dict_index] = list_with_dicts[i][key]
+                result_dict_keys_temporary[result_dict_index] = index_for_new_dict
         else:
-            # If key doesn't exist in the new dict then we just add key:value pars
-            result_dict[key] = list_with_dicts[i][key]
+            # If key doesn't exist in the new dict then we just add values to all three lists
+            result_dict_keys.append(key)
+            result_dict_values.append(list_with_dicts[i][key])
+            result_dict_keys_temporary.append(key)
+
+# In this for... statement I parse lists with keys and values to create dict
+for i in range(len(result_dict_keys)):
+    result_dict[result_dict_keys[i]] = result_dict_values[i]
 
 # Let's print our result dict
 print(result_dict)
