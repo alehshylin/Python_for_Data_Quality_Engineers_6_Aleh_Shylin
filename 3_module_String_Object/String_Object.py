3ec7af27c469640f9e3b59a98221f49b79db7ca8
alehshylin <ol564zx@gmail.com>
2022-10-03 12:18:10 +0300

    Minor changes were applied to sentence parsing, text transformation and comments section

--- 3_module_String_Object/String_Object.py
+++ 3_module_String_Object/String_Object.py
@@ -23,8 +23,9 @@ homEwork:
 # I create list to parse original text in it. Each element of this list stores one sentence from original text
 text_list = []
 
-# In this for... statement I parse original text to the list. I use dot symbol '.' as separator
-for sentence in variable_with_original_text.split('.'):
+# In this for... statement I parse original text to the list. There are possible separators. I assume, that sentence
+# ends with next symbols: '.', '..', '...', '!', '?'.
+for sentence in re.split('\.{1,3}|!|\?', variable_with_original_text):
     # And append sentence to the list
     text_list.append(sentence)
 
@@ -49,14 +50,8 @@ for i in range(len(text_list)-1):
     # not in the words (i.e. word like 'existing'). That's why I use only spaces to avoid replacing in the words like
     # 'normalize'.
     text_list[i] = re.sub(' iz ', ' is ', text_list[i])
-    # I decide to replace also "iz" expression in the sentence 'fix“iZ” with correct “is”...' because I assume that
-    # this expression is also incorrect. I do not combine replacement from line 51 and line 59 to one pattern because
-    # of the next reason: I may create pattern like '\Wiz\W$' or '[^a-zA-Z0-9_]iz[^a-zA-Z0-9_]$'. That pattern
-    # catch both 'iz' and '“iz”' expression, but in that case I need to understand what exactly pattern catch.
-    # I can do it by if... elif... statements, but I think that for that case it will be code complication.
-    # That's why I just wrote two lines which work only in a specific case instead of block of code that will work in
-    # mostly cases.
-    text_list[i] = re.sub('“iz”', ' “is” ', text_list[i])
+    # In the line 54 I add spaces to the "iz" because in original text this expression was glued with the previous word
+    text_list[i] = re.sub('“iz”', ' “iz” ', text_list[i])
     # I also notice that we have 'tex.' expression in the end of the sentence 'last iz TO calculate nuMber
     # OF Whitespace characteRS in this Tex.' I also decide to replace it by word 'text'. In this small pattern I use
     # end of the line character '$' to make sure that I not replace words that start from 'tex'. I also should use
@@ -68,20 +63,11 @@ for i in range(len(text_list)-1):
     # Then I split each words to three variables. Variable 'first' contains first word of the sentence. Variable 'last'
     # contains the last word of the sentence. Variable (or it is better to say array or *args) '*middle' contains all
     # other words (we can say words that is not first and last in the sentence). We will work with 'last'
-    # and 'middle' variables only.
+    # variable only.
     first, *middle, last = text_list[i].split()
-    # In the task requirements we need to create last sentence with last words. I assume that I need to use only words.
-    # We have one sentence with the number in the end of the sentence. In that case we should take word before it.
-    # To check if last expression of the sentence is number we can use method .isdigit().
-    if not last.isdigit():
-        # If the last expression is not a digit then we just add it to our variable 'last_sentence' with a space
-        last_sentence += last + ' '
-    else:
-        # If the last expression is digit then we take penultimate expression from the sentence and add it to our
-        # variable 'last_sentence' with a space. We should also check if the penultimate expression is digit but in
-        # that case it also will be code complication (because we have explicit text and assuming that we don't have
-        # any new text), so I decide not to check this.
-        last_sentence += middle[-1] + ' '
+    # In the task requirements we need to create last sentence with last words. We just add value from 'last' variable
+    # to our variable 'last_sentence' with a space
+    last_sentence += last + ' '
     # After all operations of original text transforming we add dot to each sentence, because we lost it when parsed
     # original text
     final_text += text_list[i] + '. '
@@ -89,7 +75,7 @@ for i in range(len(text_list)-1):
 # We still need to perform some operation with the last sentence. Firstly I delete all spaces from the start and the end
 # of the sentence
 last_sentence = last_sentence.strip()
-# Then I transform each letter of the sentect to lowecase
+# Then I transform each letter of the sentence to lowercase
 last_sentence = last_sentence.lower()
 # Then I transform first letter of the sentence to uppercase
 last_sentence = last_sentence.capitalize()
